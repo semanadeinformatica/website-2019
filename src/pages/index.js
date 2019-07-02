c4d1460159f910ddfc14617ea9007450433d3d05
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-07-02 21:35:00 +0100

    Adding example talk page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Link to="/example-talk/">Check out an example talk page</Link>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
---
+++ src/templates/talk.js
@@ -0,0 +1,31 @@
+import React from "react"
+// import Helmet from "react-helmet"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function Template({ data }) {
+  const { markdownRemark: talk } = data
+
+  return (
+    <Layout>
+      <SEO title={talk.frontmatter.title} />
+      <div>
+        <h1>{talk.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: talk.html }}></div>
+      </div>
+    </Layout>
+  )
+}
+
+export const talkQuery = graphql`
+  query TalkByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+      }
+    }
+  }
+`
