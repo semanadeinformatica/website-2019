e082b957c772baf582bd517308468ebae18e89ff
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2019-07-08 16:50:12 +0100

    Created competition component

---
+++ src/components/competition.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+const Competition = () => (
+  <div>
+    <h1>competição de programação</h1>
+    <div>
+      <p>
+        A semana de informática irá contar com uma Competição de Programação a
+        realizar-se durante todos os dias evento.
+      </p>
+      <p>
+        Serão lançados dois exercícios por dia nos primeiros três dias, de
+        dificuldade crescente.
+      </p>
+      <p>
+        A participação é individual, estará aberta a partir da sessão de
+        abertura, dia 29 de Outubro (segunda-feira), até às 14h00 de 1 de
+        Novembro (quinta-feira), e os vencedores serão anunciados na cerimónia
+        de encerramento.
+      </p>
+      <p>
+        A competição decorrá online no HackerRank, onde serão utilizados
+        mecanismos anti-cópia para detetar infracções.
+      </p>
+      <button type="button">Inscrições</button>
+    </div>
+
+    <h4>Os prémios para os vencedores são:</h4>
+    <ul>
+      <li>1ª posição: Headphones Bluetooth Sony WH-1000XM2</li>
+      <li>2ª posição: Monitor LG 24GM79G-B 144Hz</li>
+      <li>3ª posição: Raspberry Pi</li>
+    </ul>
+  </div>
+)
+
+export default Competition
