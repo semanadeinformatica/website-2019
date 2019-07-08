fcb5cd250deabd5a561c30f3a0c2de3f9753ad2e
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2019-07-08 16:19:42 +0100

    Created about component

---
+++ src/components/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+const About = () => (
+  <div>
+    <h1>Semana de informática</h1>
+    <p>
+      A semana de informática (SINF) pretende oferecer aos estudantes de cursos
+      relacionados com informática um evento que promove a aprendizagem de
+      diferentes tópicos das diversas áreas da informática, o convívio através
+      de eventos sociais e o contacto com o mundo empresarial.
+    </p>
+    <p>
+      O evento decorre entre os dias 29 de outubro e 1 de novembro de 2018, na
+      Faculdade de Engenharia da Universidade do Porto e é organizado pelo
+      Núcleo de Informática da Associação de Estudantes da Faculdade de
+      Engenharia da Universidade do Porto (NIAEFEUP).
+    </p>
+    <p>
+      Durante a SINF os estudantes poderão participar em palestras, workshops e
+      sessões de convívio e networking, contactando assim com áreas e aspectos,
+      quer de carácter técnico ou soft skills, que não são abrangidos no seu
+      percurso académico.
+    </p>
+  </div>
+)
+
+export default About
--- src/pages/sobre-nos.js
+++ src/pages/sobre-nos.js
@@ -3,33 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import About from "../components/about"
 
-const SecondPage = () => (
+const AboutUsPage = () => (
   <Layout>
     <SEO title="Sobre nós" />
-    <div>
-      <h1>Semana de informática</h1>
-      <p>
-        A semana de informática (SINF) pretende oferecer aos estudantes de
-        cursos relacionados com informática um evento que promove a aprendizagem
-        de diferentes tópicos das diversas áreas da informática, o convívio
-        através de eventos sociais e o contacto com o mundo empresarial.
-      </p>
-      <p>
-        O evento decorre entre os dias 29 de outubro e 1 de novembro de 2018, na
-        Faculdade de Engenharia da Universidade do Porto e é organizado pelo
-        Núcleo de Informática da Associação de Estudantes da Faculdade de
-        Engenharia da Universidade do Porto (NIAEFEUP).
-      </p>
-      <p>
-        Durante a SINF os estudantes poderão participar em palestras, workshops
-        e sessões de convívio e networking, contactando assim com áreas e
-        aspectos, quer de carácter técnico ou soft skills, que não são
-        abrangidos no seu percurso académico.
-      </p>
-    </div>
+    <About />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default AboutUsPage
