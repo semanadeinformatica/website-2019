dd147b1228f98548d28fef4385726dbee94a2d31
Pedrocssg <pedrokraktuga@gmail.com>
2019-07-15 20:05:05 +0100

    Added Member component, updated Team Page

--- src/pages/equipa.js
+++ src/pages/equipa.js
@@ -1,16 +1,9 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-const styles = {
-  image: {
-    width: "248px",
-    height: "248px",
-  },
-}
+import Member from "../components/member"
 
 const TeamPage = ({ data }) => (
   <Layout>
@@ -19,28 +12,7 @@ const TeamPage = ({ data }) => (
     {data.allMarkdownRemark.edges.map(({ node }) => (
       <div key={node.id}>
         <div key={node.frontmatter.name}>
-          <Img
-            fluid={node.frontmatter.img.childImageSharp.fluid}
-            style={styles.image}
-          />
-          <div>
-            <div>{node.frontmatter.name}</div>
-            <div>{node.frontmatter.role}</div>
-            <div>
-              {node.frontmatter.linkedin ? (
-                <a href={node.frontmatter.linkedin}>LinkedIn</a>
-              ) : (
-                ""
-              )}
-            </div>
-            <div>
-              {node.frontmatter.github ? (
-                <a href={node.frontmatter.github}>GitHub</a>
-              ) : (
-                ""
-              )}
-            </div>
-          </div>
+          <Member key={node.frontmatter.name} data={node.frontmatter} />
         </div>
       </div>
     ))}
