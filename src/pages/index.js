b10464fc2ff4a85382f3ff1d97ca549202ca57e4
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2019-07-18 15:34:47 +0100

    Removed unnecessary files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => (
+const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -14,48 +13,7 @@ const IndexPage = ({ data }) => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-
-    <h2>Pages</h2>
-    <div>
-      <Link to="/page-2/">Go to page 2</Link>
-    </div>
-    <div>
-      <Link to="/sobre-nos/">Sobre nós</Link>
-    </div>
-    <div>
-      <Link to="/speakers/">Speakers</Link>
-    </div>
-    <div>
-      <Link to="/competicao-programacao/">Competição</Link>
-    </div>
-
-    <h2>Talks</h2>
-    <ul>
-      {data.allMarkdownRemark.edges.map(talk => (
-        <li key={talk.node.id}>
-          <Link to={talk.node.frontmatter.path}>
-            {talk.node.frontmatter.title}
-          </Link>
-        </li>
-      ))}
-    </ul>
   </Layout>
 )
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            path
-          }
-        }
-      }
-    }
-  }
-`
 
 export default IndexPage
