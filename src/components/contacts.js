ac9c11d93c9fb4c3db958cccbac8beb0de99adb1
Pedrocssg <pedrokraktuga@gmail.com>
2019-07-18 20:09:19 +0100

    Added link span

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -4,20 +4,24 @@ const Contacts = () => (
   <div>
     <h1>Contactos</h1>
     <div>
+      <span>Email: </span>
       <a href="mailto:sinf.feup@gmail.com">sinf.feup@gmail.com</a>
     </div>
     <div>
+      <span>Facebook: </span>
       <a href="https://www.facebook.com/semanainformaticaFEUP/">
         semanainformaticaFEUP
       </a>
     </div>
     <div>
+      <span>LinkedIn: </span>
       <a href="https://www.linkedin.com/company/semanainformaticafeup/">
         semanainformaticafeup
       </a>
     </div>
     <div>
-      <p>Faculdade de Engenharia da Universidade do Porto</p>
+      <span>Localização: </span>
+      <span>Faculdade de Engenharia da Universidade do Porto</span>
     </div>
   </div>
 )
