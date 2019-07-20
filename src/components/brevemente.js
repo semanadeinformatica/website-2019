6827af822e03a5f37bcd7f0dacd16bf6d605226f
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-07-20 12:58:54 +0100

    Background and banner

---
+++ src/components/brevemente.js
@@ -0,0 +1,16 @@
+import React from "react"
+import SoonStyles from "../styles/soon.module.css"
+import Icon from "../images/svg/gradient-outline.inline.svg"
+const Brevemente = () => {
+  return (
+    <div className={SoonStyles.banner}>
+      <Icon className={SoonStyles.banner_icon} />
+      <h1 className={SoonStyles.title}>Brevemente</h1>
+      <a className={SoonStyles.link} href="https://sinf.pt/">
+        Edição Anterior
+      </a>
+    </div>
+  )
+}
+
+export default Brevemente
