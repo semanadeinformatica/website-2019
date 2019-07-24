1705c37af2850da99d0e12f69d03b6d31c2acee3
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-07-24 10:47:48 +0100

    Added logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 
+import "bootstrap/dist/css/bootstrap.min.css"
+
 import SEO from "../components/seo"
 import SoonStyles from "../styles/soon.module.css"
 import Brevemente from "../components/brevemente"
 import Info from "../components/info"
 import { Container, Row, Col } from "reactstrap"
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import Icon from "../images/svg/logo_sinf_comp.inline.svg"
 
 const IndexPage = () => (
   <div className={SoonStyles.background}>
@@ -14,8 +16,10 @@ const IndexPage = () => (
     <Brevemente />
 
     <Container fluid className={SoonStyles.content_container}>
-      <Row>
-        <Col></Col>
+      <Row className={SoonStyles.top_row}>
+        <Col>
+          <Icon alt="Logo" className={SoonStyles.logo} />
+        </Col>
       </Row>
       <Row className={SoonStyles.bottom_row}>
         <Col>
