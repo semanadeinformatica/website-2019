e6cd8a27308ba0032624716e0fc3edecb649aeed
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-08-22 10:31:24 +0100

    Migrated icons to react-icons

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -1,41 +1,38 @@
 import React from "react"
 import { Container, Col, Row } from "reactstrap"
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
-  faFacebookSquare,
-  faLinkedin,
-} from "@fortawesome/free-brands-svg-icons"
-import { faEnvelope, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"
+  FaEnvelope,
+  FaLinkedin,
+  FaFacebookSquare,
+  FaMapMarkerAlt,
+} from "react-icons/fa"
 
 import ContactStyles from "../../styles/contacts.module.css"
 
 const links = [
   {
-    icon: faEnvelope,
+    icon: <FaEnvelope />,
     href: "mailto:geral@sinf.pt",
     text: "geral@sinf.pt",
   },
   {
-    icon: faLinkedin,
+    icon: <FaLinkedin />,
     href: "https://www.linkedin.com/company/semanainformaticafeup/",
     text: "sinffeup",
   },
   {
-    icon: faFacebookSquare,
+    icon: <FaFacebookSquare />,
     href: "https://facebook.com/semanainformaticaFEUP",
     text: "sinfFEUP",
   },
 ]
 
 const location = {
-  icon: faMapMarkerAlt,
+  icon: <FaMapMarkerAlt />,
   href:
     "https://www.google.com/maps/place/FEUP+-+Faculdade+de+Engenharia+da+Universidade+do+Porto/@41.17794,-8.597688,15z/data=!4m5!3m4!1s0x0:0x8b1e4a0bcdacc840!8m2!3d41.1779401!4d-8.5976876?hl=en-US",
   text: "R. Dr. Roberto Frias, 4200-465 Porto, Portugal",
 }
-
-const icon_size = "2x"
 
 const Contacts = () => (
   <div>
@@ -52,11 +49,7 @@ const Contacts = () => (
               <Col key={index}>
                 <Row>
                   <a href={link.href} className={ContactStyles.link}>
-                    <FontAwesomeIcon
-                      icon={link.icon}
-                      className={ContactStyles.icon}
-                      size={icon_size}
-                    />
+                    <span className={ContactStyles.icon}>{link.icon}</span>
                     {link.text}
                   </a>
                 </Row>
@@ -66,11 +59,7 @@ const Contacts = () => (
         </Col>
         <Col className={ContactStyles.info_col}>
           <a href={location.href} className={ContactStyles.location_link}>
-            <FontAwesomeIcon
-              icon={location.icon}
-              className={ContactStyles.icon}
-              size={icon_size}
-            />
+            <span className={ContactStyles.icon}>{location.icon}</span>
             {location.text}
           </a>
         </Col>
