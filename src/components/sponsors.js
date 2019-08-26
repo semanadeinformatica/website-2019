0df8a092aedd82f4ecbb76fb876189539167573e
Pedrocssg <pedrokraktuga@gmail.com>
2019-08-26 18:19:01 +0100

    Sponsors heading style implemented

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import SingleSponsor from "./single-sponsor"
+import sponsorsStyles from "../styles/sponsors.module.css"
 
 const getSponsors = (data, type) => {
   return data.allMarkdownRemark.edges
@@ -33,23 +34,30 @@ const Sponsors = () => {
   `)
 
   return (
-    <div>
-      <h1>Patrocínios</h1>
+    <section id="sponsors" className={sponsorsStyles.sponsorsSection}>
+      <h2 className={sponsorsStyles.h2}>
+        Sponsors
+        <hr className={sponsorsStyles.headingLine} />
+      </h2>
 
-      {getSponsors(data, "main")}
+      <div>
+        <h1>Patrocínios</h1>
 
-      <h2>Patrocínio gold</h2>
-      {getSponsors(data, "gold")}
+        {getSponsors(data, "main")}
 
-      <h2>Patrocínio silver</h2>
-      {getSponsors(data, "silver")}
+        <h2>Patrocínio gold</h2>
+        {getSponsors(data, "gold")}
 
-      <h2>Patrocínio bronze</h2>
-      {getSponsors(data, "bronze")}
+        <h2>Patrocínio silver</h2>
+        {getSponsors(data, "silver")}
 
-      <h2>Parceiros</h2>
-      {getSponsors(data, "partner")}
-    </div>
+        <h2>Patrocínio bronze</h2>
+        {getSponsors(data, "bronze")}
+
+        <h2>Parceiros</h2>
+        {getSponsors(data, "partner")}
+      </div>
+    </section>
   )
 }
 
