c7b1a019fbd2a787992f0238e0c0694d5a6ba039
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-08-28 11:38:43 +0100

    Version and old code clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,49 +11,6 @@ const IndexPage = ({ data }) => (
   <div>
     <Navbar />
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-
-    <h2>Pages</h2>
-    <div>
-      <Link to="/page-2/">Go to page 2</Link>
-    </div>
-    <div>
-      <Link to="/sobre-nos/">Sobre nós</Link>
-    </div>
-    <div>
-      <Link to="/speakers/">Speakers</Link>
-    </div>
-    <div>
-      <Link to="/competicao-programacao/">Competição</Link>
-    </div>
-    <div>
-      <Link to="/program/">Programa</Link>
-    </div>
-    <div>
-      <Link to="/equipa/">Equipa</Link>
-    </div>
-    <div>
-      <Link to="/patrocinios/">Patrocínios</Link>
-    </div>
-    <div>
-      <Link to="/contactos/">Contactos</Link>
-    </div>
-
-    <h2>Talks</h2>
-    <ul>
-      {data.allMarkdownRemark.edges.map(talk => (
-        <li key={talk.node.id}>
-          <Link to={talk.node.frontmatter.path}>
-            {talk.node.frontmatter.title}
-          </Link>
-        </li>
-      ))}
-    </ul>
   </div>
 )
 
