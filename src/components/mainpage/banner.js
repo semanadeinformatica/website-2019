1091dff18c0072a0f212ef7c3431237113eec195
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-09-06 10:07:16 +0100

    Absolute position for banner elements

--- src/components/mainpage/banner.js
+++ src/components/mainpage/banner.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Container, Col, Row } from "reactstrap"
 
 import BasicInfo from "./basic-info"
 
@@ -9,16 +8,10 @@ import Icon from "../../images/svg/logo_sinf_comp.inline.svg"
 const Banner = () => {
   return (
     <div className={BannerStyles.banner}>
-      <Container className={BannerStyles.banner_info_container} fluid>
-        <Row>
-          <Col className={BannerStyles.banner_info}>
-            <Icon className={BannerStyles.icon} />
-            <div className={BannerStyles.basic_info}>
-              <BasicInfo />
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <Icon className={BannerStyles.icon} />
+      <div className={BannerStyles.basic_info}>
+        <BasicInfo />
+      </div>
     </div>
   )
 }
