c24fe3c62b41f8baf01795bc9adc606805d9bf8e
Pedrocssg <pedrokraktuga@gmail.com>
2019-09-18 22:16:26 +0100

    Small improvement on single sponsor code quality

--- src/components/mainpage/sponsors/single-sponsor.js
+++ src/components/mainpage/sponsors/single-sponsor.js
@@ -3,16 +3,10 @@ import Img from "gatsby-image"
 import sponsorStyles from "../../../styles/single-sponsor.module.css"
 
 const SingleSponsor = ({ sponsor, type }) => {
-  let assignType
-  if (type === "main") assignType = sponsorStyles.main
-  else if (type === "gold") assignType = sponsorStyles.gold
-  else if (type === "silver") assignType = sponsorStyles.silver
-  else assignType = sponsorStyles.bronze
-
   return (
     <>
       <Img
-        className={[sponsorStyles.image, assignType].join(" ")}
+        className={[sponsorStyles.image, sponsorStyles[type]].join(" ")}
         fluid={sponsor.img.childImageSharp.fluid}
       />
     </>
