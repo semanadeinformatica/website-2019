53d080a148aaff42fdb640bba2682d47c6e97047
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-09-25 14:24:46 +0100

    Introduce styling for the participate component

--- src/components/talk/participate.js
+++ src/components/talk/participate.js
@@ -1,10 +1,14 @@
 import React from "react"
 import { Row } from "reactstrap"
 
+import ParticipateStyles from "../../styles/talk/participate.module.css"
+
 const Participate = ({ href }) => {
   return (
-    <Row>
-      <a href={href}>Participa</a>
+    <Row className={ParticipateStyles.container}>
+      <a href={href} className={ParticipateStyles.link}>
+        Participar
+      </a>
     </Row>
   )
 }
