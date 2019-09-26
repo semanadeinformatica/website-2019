86fcd875ac3084c8fc695c864dcb96d5ee28965c
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-09-26 15:51:53 +0100

    Reactstrap containers added

--- src/components/competition.js
+++ src/components/competition.js
@@ -1,29 +1,22 @@
 import React from "react"
+import { Container, Row, Col } from "reactstrap"
+
+// import competitionStyles from "../styles/competition/competition.module.css"
 
 const Competition = () => (
-  <div>
-    <h1>competição de programação</h1>
-    <div>
-      <p>
+  <Container>
+    <Row>
+      <Col>
         A semana de informática irá contar com uma Competição de Programação a
-        realizar-se durante todos os dias evento.
-      </p>
-      <p>
-        Serão lançados dois exercícios por dia nos primeiros três dias, de
-        dificuldade crescente.
-      </p>
-      <p>
-        A participação é individual, estará aberta a partir da sessão de
-        abertura, dia 29 de Outubro (segunda-feira), até às 14h00 de 1 de
-        Novembro (quinta-feira), e os vencedores serão anunciados na cerimónia
-        de encerramento.
-      </p>
-      <p>
-        A competição decorrá online no HackerRank, onde serão utilizados
-        mecanismos anti-cópia para detetar infracções.
-      </p>
-      <button type="button">Inscrições</button>
-    </div>
+        realizar-se durante todos os dias evento. Serão lançados dois exercícios
+        por dia nos primeiros três dias, de dificuldade crescente. A
+        participação é individual, estará aberta a partir da sessão de abertura,
+        dia 29 de Outubro (segunda-feira), até às 14h00 de 1 de Novembro
+        (quinta-feira), e os vencedores serão anunciados na cerimónia de
+        encerramento. A competição decorrá online no HackerRank, onde serão
+        utilizados mecanismos anti-cópia para detetar infracções.
+      </Col>
+    </Row>
 
     <h4>Os prémios para os vencedores são:</h4>
     <ul>
@@ -46,7 +39,7 @@ const Competition = () => (
         </a>
       </li>
     </ul>
-  </div>
+  </Container>
 )
 
 export default Competition
