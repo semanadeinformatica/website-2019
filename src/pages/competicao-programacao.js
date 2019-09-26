16b5ed6720b842cd93d71774226d4240dd461368
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-09-26 14:24:24 +0100

    Added Page Banner to Competition Page

--- src/pages/competicao-programacao.js
+++ src/pages/competicao-programacao.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/common/layout"
 import SEO from "../components/common/seo"
+import Layout from "../components/common/layout"
+
 import Competition from "../components/competition"
+import PageBanner from "../components/utils/page_banner"
+
+import Icon from "../images/svg/competicao.inline.svg"
 
 const CompetitionPage = () => (
   <Layout>
     <SEO title="Competição de programação" />
+    <PageBanner>
+      <Icon />
+    </PageBanner>
     <Competition />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
