7aaa28d5d5859a31441fba5d4aab241d2087aa5f
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-09-28 13:18:05 +0100

    Adding theme header to the program nav

--- src/components/program/ProgramNav.js
+++ src/components/program/ProgramNav.js
@@ -5,30 +5,39 @@ import DayNavItem from "./DayNavItem"
 import programStyles from "../../styles/program/program.module.css"
 
 const ProgramNav = ({ days, activeTab, toggle }) => (
-  <div>
-    <Nav className="justify-content-center">
-      {days.map((day, i) => {
-        return (
-          <NavItem
-            key={day.node.id}
-            className={
-              activeTab.date === day.node.date
-                ? [programStyles.navItemWrapper, programStyles.active].join(" ")
-                : programStyles.navItemWrapper
-            }
-          >
-            <NavLink onClick={() => toggle(day.node)}>
-              <DayNavItem
-                date={day.node.date}
-                icon={day.node.icon}
-                day={i + 1}
-                active={activeTab.date === day.node.date}
-              />
-            </NavLink>
-          </NavItem>
-        )
-      })}
-    </Nav>
+  <div className={programStyles.programNavWrapper}>
+    <div className={programStyles.programNavContainer}>
+      <Nav className={programStyles.programNav}>
+        {days.map((day, i) => {
+          return (
+            <NavItem
+              key={day.node.id}
+              className={
+                activeTab.date === day.node.date
+                  ? [programStyles.navItemWrapper, programStyles.active].join(
+                      " "
+                    )
+                  : programStyles.navItemWrapper
+              }
+            >
+              <NavLink onClick={() => toggle(day.node)}>
+                <DayNavItem
+                  date={day.node.date}
+                  icon={day.node.icon}
+                  day={i + 1}
+                  active={activeTab.date === day.node.date}
+                />
+              </NavLink>
+            </NavItem>
+          )
+        })}
+      </Nav>
+      <div className={programStyles.themeHeader}>
+        <h3 className={programStyles.themeHeading}>
+          <span className={programStyles.theme}>Tema:</span> {activeTab.theme}
+        </h3>
+      </div>
+    </div>
   </div>
 )
 
