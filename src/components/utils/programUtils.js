a00ae7467e9a0b963b7d12d639e7db521c049a5e
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-09-29 17:34:52 +0100

    Centering schedule and finishing up mobile styling

--- src/components/utils/programUtils.js
+++ src/components/utils/programUtils.js
@@ -18,24 +18,33 @@ export const TimePlace = ({ start_time, end_time, place }) => (
   </p>
 )
 
-export const Speakers = ({ speakers, path }) => (
-  <>
-    <h3 className={eventsStyles.speakers}>
-      <Link to={path}>
-        {speakers.map((speaker, i) =>
-          i > 0 ? `, ${speaker.name}` : speaker.name
-        )}
-      </Link>
-    </h3>
-    <h5 className={eventsStyles.occupations}>
-      {speakers.map(({ occupations }, i) =>
-        i > 0
-          ? `, ${occupations[0].what} @ ${occupations[0].where}`
-          : `${occupations[0].what} @ ${occupations[0].where}`
-      )}
-    </h5>
-  </>
-)
+export const Speakers = ({ speakers, path }) => {
+  const printOccupations = speakers => {
+    let occupations = speakers.map(
+      ({ occupations }) => `${occupations[0].what} @ ${occupations[0].where}`
+    )
+
+    occupations = occupations.filter(
+      (occupation, index, self) => self.indexOf(occupation) === index
+    )
+    return occupations.map((occupation, index) =>
+      index > 0 ? `, ${occupation}` : occupation
+    )
+  }
+
+  return (
+    <>
+      <h3 className={eventsStyles.speakers}>
+        <Link to={path}>
+          {speakers.map((speaker, i) =>
+            i > 0 ? `, ${speaker.name}` : speaker.name
+          )}
+        </Link>
+      </h3>
+      <h5 className={eventsStyles.occupations}>{printOccupations(speakers)}</h5>
+    </>
+  )
+}
 
 export const DescriptionToggler = ({
   id,
