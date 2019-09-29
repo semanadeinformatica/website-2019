82f89a55b6afc89931f543cbc813cad2aa1a58d0
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2019-09-29 21:06:17 +0100

    Navbar on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 import SEO from "../components/common/seo"
 
+import Layout from "../components/common/layout"
 import Face from "../images/svg/not_found_face.inline.svg"
 import Numbers from "../images/svg/not_found_numbers.inline.svg"
 
 import NotFoundStyles from "../styles/404.module.css"
 
 const NotFoundPage = () => (
-  <React.Fragment>
+  <Layout>
     <SEO title="404: Not found" />
     <div className={NotFoundStyles.main}>
       <Face className={NotFoundStyles.face} />
@@ -18,7 +19,7 @@ const NotFoundPage = () => (
         Voltar
       </Link>
     </div>
-  </React.Fragment>
+  </Layout>
 )
 
 export default NotFoundPage
