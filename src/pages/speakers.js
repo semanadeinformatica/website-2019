0ad7d275b29f62a5a423fd4b9969af4bd0df8f62
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2019-10-01 23:58:10 +0100

    Removed github from query + image overflow fix

--- src/pages/speakers.js
+++ src/pages/speakers.js
@@ -46,7 +46,6 @@ export const pageQuery = graphql`
                 linkedin
                 website
                 twitter
-                github
               }
               day
               path
